870291320d4d11c0c7ece391e6a166784e3198a3
theonz <51724616+TheoBraithwaite@users.noreply.github.com>
2025-04-02 12:49:39 +1300

    Maqueen robot completes first turn.

--- main.ts
+++ main.ts
@@ -2,19 +2,20 @@ function lineTracking () {
     if (reverse == false) {
         if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
-            basic.pause(50)
+            basic.pause(200)
         } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 0) {
-            basic.pause(50)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
-            maqueen.motorStop(maqueen.Motors.M2)
-        } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
-            basic.pause(50)
-            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
-            maqueen.motorStop(maqueen.Motors.M1)
-        } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+            basic.pause(100)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
+        } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 0 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+            basic.pause(100)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+        } else if (maqueen.readPatrol(maqueen.Patrol.PatrolLeft) == 1 && maqueen.readPatrol(maqueen.Patrol.PatrolRight) == 1) {
+            maqueen.motorStop(maqueen.Motors.All)
         }
-        basic.pause(200)
         lineTracking()
     }
 }
@@ -24,11 +25,16 @@ function funcReverse () {
         reverseCount += 1
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 20)
     } else if (reverseCount >= 1) {
-        if (timerReverse >= 2) {
+        if (timerReverse >= 3) {
             maqueen.motorStop(maqueen.Motors.All)
             maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
-            basic.pause(1000)
+            basic.pause(1250)
             reverse = false
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+            basic.pause(1250)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+            basic.pause(500)
             lineTracking()
         }
     }
@@ -43,7 +49,6 @@ reverseCount = 0
 maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
 loops.everyInterval(500, function () {
     basic.showNumber(timerReverse)
-    basic.pause(500)
     if (reverse == true) {
         timerReverse += 1
         funcReverse()
